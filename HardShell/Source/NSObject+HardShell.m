41b479e408594b2e0c13cbbc38292a0525af3821
Peng <guoleii@gmail.com>
2017-03-01 13:23:15 +0800

    give default types for class_addMethod

--- HardShell/Source/NSObject+HardShell.m
+++ HardShell/Source/NSObject+HardShell.m
@@ -14,7 +14,7 @@
 /* TODO: 1. what if forwardInvocation: is rewritten by original class
  *       2. what if class_addMethod fails?
  *
-*/
+ */
 - (id)forwardingTargetForSelector:(SEL)aSelector {
     
     NSString *originClass = NSStringFromClass([self class]);
@@ -26,14 +26,12 @@
     if(stubClass == nil){
         stubClass = objc_allocateClassPair([NSObject class], [stubClassName UTF8String], 0);
     }
-    Method methodForSelector = class_getInstanceMethod([self class],aSelector);
-    const char *types = method_getTypeEncoding(methodForSelector);
     IMP imp = imp_implementationWithBlock(^(__unsafe_unretained id self, va_list argp) {
         return 0;
         
     });
+    class_addMethod(stubClass, aSelector, imp, "v@:");
     
-    class_addMethod(stubClass, aSelector, imp, types);
     id stub = [[stubClass alloc] init];
     
     return stub;
