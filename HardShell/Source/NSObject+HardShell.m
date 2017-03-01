f1a6607835113072d2bf32a692707d9354d6fee3
Peng <guoleii@gmail.com>
2017-03-01 12:30:36 +0800

    fix types for class_addMethod

--- HardShell/Source/NSObject+HardShell.m
+++ HardShell/Source/NSObject+HardShell.m
@@ -22,14 +22,15 @@
     if(stubClass == nil){
         stubClass = objc_allocateClassPair([NSObject class], [stubClassName UTF8String], 0);
     }
-    Method forwardingTargetForSelector = class_getInstanceMethod([NSObject class],@selector(forwardingTargetForSelector:));
-    const char *types = method_getTypeEncoding(forwardingTargetForSelector);
+    Method methodForSelector = class_getInstanceMethod([self class],aSelector);
+    const char *types = method_getTypeEncoding(methodForSelector);
     IMP imp = imp_implementationWithBlock(^(__unsafe_unretained id self, va_list argp) {
         return 0;
         
     });
-    class_addMethod(stubClass, aSelector, imp, types);
     
+    // TODO: what if class_addMethod fails?
+    class_addMethod(stubClass, aSelector, imp, types);
     id stub = [[stubClass alloc] init];
     
     return stub;
